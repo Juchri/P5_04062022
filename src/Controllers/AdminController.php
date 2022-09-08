ad6856fc8378057e15962243f7200818430c2c07
Juchri <juliette.chri@gmail.com>
2022-09-08 21:09:49 +0200

    admin commentaires ok

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -1,20 +1,48 @@
 <?php
 namespace App\Controllers;
 
+use App\Models\CommentsModel;
 use App\Models\UsersModel;
 
 class AdminController extends Controller
 {
     public function index() {
         if($this->isAdmin()){
-            $this->twig->display('admin/index.html.twig');
+            $this->twig->display('admin/index.html.twig');     
         }
     }
 
-    public function admin()
-    {
-        $this->twig->display('admin/index.html.twig');     
+    public function posts() {
+        if($this->isAdmin()){
+            $this->twig->display('admin/posts/index.html.twig');     
+        }
     }
+
+    public function comments() {
+        if($this->isAdmin()){
+            $commentModel = new CommentsModel;
+
+            if (isset($_POST['valider'])) {
+                $commentId = $_POST['commentId'];
+                $commentArray = $commentModel->find($commentId);
+                if($commentArray){
+                    $comment = $commentModel->hydrate($commentArray);
+                    $comment->setIsValid(1);
+                    $comment->update();
+                }
+                header(('Location: index.php?p=admin/comments'));
+            } elseif (isset($_POST['supprimer'])) {
+                $commentId = $_POST['commentId'];
+                $commentModel->delete($commentId);
+                header(('Location: index.php?p=admin/comments'));
+            }
+
+            $allComments = $commentModel->findAll();
+            $this->twig->display('admin/comments/index.html.twig', ['allComments' => $allComments]);     
+        }
+    }
+
+
 
         /**
      * Vérifie si on est admin
@@ -29,7 +57,7 @@ class AdminController extends Controller
         }else{
             // On n'est pas admin
             $_SESSION['erreur'] = "Vous n'avez pas accès à cette zone";
-            header('Location: /');
+            header('Location: /post');
             exit;
         }
     }
